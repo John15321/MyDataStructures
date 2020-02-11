955a7c4f08024c15dd9d0c67c3f0e46f7052e45d
John15321 <56229312+John15321@users.noreply.github.com>
2020-02-11 20:20:36 +0100

    .

--- MyLinkedLists/main.cpp
+++ MyLinkedLists/main.cpp
@@ -6,25 +6,21 @@ int main()
     list<int> node2(2);
     list<int> node3(3);
 
-    std::cout<<"------------------"<<std::endl;
+    std::cout << "------------------" << std::endl;
     // Appending
     node1.Append(node2);
-    std::cout<<"------------------"<<std::endl;
+    std::cout << "------------------" << std::endl;
     node2.Append(node3);
-    std::cout<<"------------------"<<std::endl;
-    
-    std::cout<<"FindLengthIter(): "<<
-    node1.FindLengthIter()<<std::endl;
-    std::cout<<"------------------"<<std::endl;
-    std::cout<<"FindLengthRec(): "<<
-    node1.FindLengthRec()<<std::endl;
-    std::cout<<"------------------"<<std::endl;
+    std::cout << "------------------" << std::endl;
+
+    std::cout << "FindLengthIter(): " << node1.FindLengthIter() << std::endl;
+    std::cout << "------------------" << std::endl;
+    std::cout << "FindLengthRec(): " << node1.FindLengthRec() << std::endl;
+    std::cout << "------------------" << std::endl;
     int key;
-    std::cin>>key;  
-    std::cout<<"If "<<key<<" inside: "<<
-    (node1.SearchIter(key)== 1 ? "True":"False")<<std::endl;
-    std::cout<<"------------------"<<std::endl;
-    std::cout<<"If "<<key<<" inside: "<<
-    (node1.SearchRec(key)== 1 ? "True":"False")<<std::endl;
-    std::cout<<"------------------"<<std::endl;
+    std::cin >> key;
+    std::cout << "If " << key << " inside: " << (node1.SearchIter(key) == 1 ? "True" : "False") << std::endl;
+    std::cout << "------------------" << std::endl;
+    std::cout << "If " << key << " inside: " << (node1.SearchRec(key) == 1 ? "True" : "False") << std::endl;
+    std::cout << "------------------" << std::endl;
 }
