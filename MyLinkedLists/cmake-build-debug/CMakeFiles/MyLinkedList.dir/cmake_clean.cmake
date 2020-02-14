e0bbbcf2d9f30331732a7665b1b370ab96ccfdbf
john <johngadsden1775@gmail.com>
2020-02-15 00:46:22 +0100

    aaaaa

---
+++ MyLinkedLists/cmake-build-debug/CMakeFiles/MyLinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyLinkedList.dir/main.cpp.o"
+  "MyLinkedList"
+  "MyLinkedList.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyLinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
